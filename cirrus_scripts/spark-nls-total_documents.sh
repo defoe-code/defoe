be5627b24b60ea6d9c80c2d000f428a26a5d0dd8
defoe-code <rosaf4@indy2-login0.ib0.icexa.epcc.ed.ac.uk>
2020-04-22 15:38:18 +0100

    adding new scripts for running HPC text mining queries

---
+++ cirrus_scripts/spark-nls-total_documents.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+#This script if for using after the Spark-Cluster is running. 
+#It submits a Spark text mining query to the Spark cluster.
+
+hostmaster=$1
+echo "Master Node" $hostmaster
+export SPARK_HOME=${HOME}/spark-2.4.0-bin-hadoop2.7
+module load anaconda/python3
+source activate cirrus-py36
+
+#Getting the Number of cores ( NUM WORKERS * 36 ) to use for running the Spark Textmining application
+NUM=$(wc -l $HOME/bash_scripts/worker.log)
+NUMWORKERS=$(echo $NUM| cut -d' ' -f1)
+NUMCORES=$( expr 36 '*' "$NUMWORKERS")
+
+echo "Number of cores for this query is" $NUMCORES
+
+#Submtting a Spark Text Mining query (total words) to the Spark Master ($hostmaster), using all the cores available in the Spark Cluster ($NUMCORES)
+cd $HOME/defoe
+
+
+$SPARK_HOME/bin/spark-submit --master spark://$hostmaster:7077 --executor-memory 60g --py-files defoe.zip defoe/run_query.py nls_total.txt nls defoe.nls.queries.total_documents  -r results_total_documents -n $NUMCORES > log.txt 
+
+
+echo "End of the text mining query using" $NUMCORES " cores"
