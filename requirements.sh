16e02a22e2e0d5762201a7acdb1bd525afd7139d
rosafilgueira <rosa.filgueira@ed.ac.uk>
2020-07-28 18:58:57 +0000

    requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -14,6 +14,7 @@ conda install --yes pathlib
 conda install -c conda-forge spacy
 python -m spacy download en
 python -m spacy download en_core_web_lg
+bash scripts/download_ntlk_corpus.sh
 
 #conda install --yes pip git
 #pip install git+https://github.com/alan-turing-institute/defoe.git
