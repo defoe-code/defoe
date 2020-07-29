ba243325f798ecca0e5a500675ec36809d1c507a
rosafilgueira <rosa.filgueira@ed.ac.uk>
2020-07-29 03:43:54 +0000

    update

--- defoe/long_s_fix/long_s.py
+++ defoe/long_s_fix/long_s.py
@@ -2,15 +2,15 @@ import os
 import subprocess
 import re
 
-DEFOE_PATH="/lustre/home/sc048/rosaf4/defoe/"
-OS="sys-i386-64"
+defoe_path ="/home/rosa_filgueira_vicente/defoe/"
+os = "sys-i386-64"
 
 def longsfix_sentence(sentence):
     print("Original sentence: %s" %sentence)
     if "'" in sentence:
         sentence=sentence.replace("'", "\'\\\'\'")
 
-    cmd = 'printf \'%s\' \''+ sentence + '\' | '+ DEFOE_PATH + 'defoe/long_s_fix/' + OS + '/lxtransduce -l spelling='+ DEFOE_PATH+ 'defoe/long_s_fix/f-to-s.lex '+ DEFOE_PATH+ 'defoe/long_s_fix/fix-spelling.gr'
+    cmd = 'printf \'%s\' \''+ sentence + '\' | '+ defoe_path + 'defoe/long_s_fix/' + os + '/lxtransduce -l spelling='+ defoe_path+ 'defoe/long_s_fix/f-to-s.lex '+ defoe_path+ 'defoe/long_s_fix/fix-spelling.gr'
 
     try:
         proc=subprocess.Popen(cmd.encode('utf-8'), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -21,7 +21,6 @@ def longsfix_sentence(sentence):
             stdout_value = sentence
         else:
              stdout_value = stdout
-        proc.terminate()
 
         fix_s= stdout_value.decode('utf-8').split('\n')[0]
     except:
