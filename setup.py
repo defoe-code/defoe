c98d1adf5f2538875590c9785af083f834e5145e
rosafilgueira <rosa.filgueira@ed.ac.uk>
2020-07-30 00:36:11 +0000

    setup

--- setup.py
+++ setup.py
@@ -5,16 +5,26 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="defoe",
-    version="0.0.1",
+    version="0.0.2",
     author="ATI-SE",
     description="Analysis of historical books and newspapers data",
+    keywords="text mining, historical, defoe, workflows",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/alan-turing-institute/defoe",
+    url="https://github.com/defoe-code/defoe",
     packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'defoe-submit = defoe-submit:main',
+        ]
+    },
     classifiers=[
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+
+    python_requires = '>=3.6',
 )
+
+
