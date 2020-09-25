30a94401275006c64cfc931c217ad1c8e14a963d
defoe-code <rosa.filgueira.vicente@gmail.com>
2020-09-25 15:42:15 +0100

    cirrus scripts - not needed

--- cirrus_scripts/bash_scripts/start_master.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-module load anaconda/python3
-source activate cirrus-py36
-export SPARK_HOME=${HOME}/spark-2.4.0-bin-hadoop2.7
-cd $SPARK_HOME/
-
-sbin/start-master.sh
-echo "Started spark Master $HOSTNAME"
