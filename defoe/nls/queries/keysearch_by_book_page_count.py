04c9036698793db0e8f8e663d07dae4a8645e02f
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2020-09-28 16:33:34 +0100

    Update keysearch_by_book_page_count.py

--- defoe/nls/queries/keysearch_by_book_page_count.py
+++ defoe/nls/queries/keysearch_by_book_page_count.py
@@ -21,6 +21,8 @@ def do_query(archives, config_file=None, logger=None, context=None):
     
     Also the config_file can indicate the preprocess treatment, along with the defoe
     path, and the type of operating system.
+    
+    If a term appears several times in the same page, I still count as “1”.
 
     Example:
         '''Twas on the morn of sweet May Day':
