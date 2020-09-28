e3c0f6fc3b0edfbea0efb0259d8e259e5d36bbab
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2020-09-28 17:55:23 +0100

    Update target_keysearch_by_year_filter_date.py

--- defoe/papers/queries/target_keysearch_by_year_filter_date.py
+++ defoe/papers/queries/target_keysearch_by_year_filter_date.py
@@ -51,7 +51,7 @@ def do_query(issues, config_file=None, logger=None, context=None):
           ...
         }
 
-    :param archives: RDD of defoe.nls.archive.Archive
+    :param issues: RDD of defoe.papers.issue.Issue
     :type archives: pyspark.rdd.PipelinedRDD
     :param config_file: query configuration file
     :type config_file: str or unicode
