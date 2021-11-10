fef726f407e884d6ce18657e64b53bdd8492a500
rosafilgueira <rosa.filgueira@ed.ac.uk>
2021-11-10 09:53:25 +0000

    others

---
+++ others/legacy.sh
@@ -0,0 +1,16 @@
+set -x
+
+
+spark-submit --driver-memory 50g --py-files defoe.zip defoe/run_query.py hdfs_data_3ed.txt hdfs defoe.hdfs.queries.keysearch_articles_by_year_details queries/slavery.yml -r trade_legacy_slavery_eb_articles_3_edition.txt  -n 34
+
+spark-submit --driver-memory 50g --py-files defoe.zip defoe/run_query.py hdfs_data_4ed.txt hdfs defoe.hdfs.queries.keysearch_articles_by_year_details queries/slavery.yml -r trade_legacy_slavery_eb_articles_4_edition.txt  -n 34
+
+spark-submit --driver-memory 50g --py-files defoe.zip defoe/run_query.py hdfs_data_5ed.txt hdfs defoe.hdfs.queries.keysearch_articles_by_year_details queries/slavery.yml -r trade_legacy_slavery_eb_articles_5_edition.txt  -n 34
+
+spark-submit --driver-memory 50g --py-files defoe.zip defoe/run_query.py hdfs_data_6ed.txt hdfs defoe.hdfs.queries.keysearch_articles_by_year_details queries/slavery.yml -r trade_legacy_slavery_eb_articles_6_edition.txt  -n 34
+
+spark-submit --driver-memory 50g --py-files defoe.zip defoe/run_query.py hdfs_data_7ed.txt hdfs defoe.hdfs.queries.keysearch_articles_by_year_details queries/slavery.yml -r trade_legacy_slavery_eb_articles_7_edition.txt  -n 34
+
+spark-submit --driver-memory 50g --py-files defoe.zip defoe/run_query.py hdfs_data_8ed.txt hdfs defoe.hdfs.queries.keysearch_articles_by_year_details queries/slavery.yml -r trade_legacy_slavery_eb_articles_8_edition.txt  -n 34
+
+spark-submit --driver-memory 50g --py-files defoe.zip defoe/run_query.py hdfs_data_sup.txt hdfs defoe.hdfs.queries.keysearch_articles_by_year_details queries/slavery.yml -r trade_legacy_slavery_eb_articles_4_5_6_suplements.txt  -n 34
