9c343179a54331203ea4024ed3380b8baab3792b
rfilguei <r.filgueira@epcc.ed.ac.uk>
2022-02-21 11:22:49 +0000

    improved

--- defoe/sparql/queries/publication_normalized.py
+++ defoe/sparql/queries/publication_normalized.py
@@ -5,11 +5,13 @@ Use this query ONLY for normalizing the EB articles stored in the Knowledge Grap
 
 from operator import add
 from defoe import query_utils
-from defoe.sparql.query_utils import get_articles_list_matches, blank_as_null, get_articles_text_matches
+from defoe.sparql.query_utils import blank_as_null
 from pyspark.sql import SQLContext
-from pyspark.sql.functions import col, when
+from pyspark.sql.functions import col, when, count, sum
+from pyspark.ml.feature import SQLTransformer
 import yaml, os
 from functools import partial, reduce
+
 
 def do_query(df, config_file=None, logger=None, context=None):
     """
@@ -19,9 +21,20 @@ def do_query(df, config_file=None, logger=None, context=None):
     Returns result of form:
 
         {
-          <YEAR>: [<NUM_DOCUMENTS>, <NUM_WORDS>],
+          <YEAR>: [<NUM_VOLS>, <NUM_PAGES>, <NUM_TERMS>, <NUM_WORDS>],
           ...
         }
+    Example:
+      '1771':
+      	- 3
+      	- 2722
+      	- 8923
+      	- 1942064
+      '1773':
+     	- 3
+     	- 2740
+     	- 9187
+     	- 1923682
 
     :param archives: RDD of defoe.es.archive.Archive
     :type archives: pyspark.rdd.PipelinedRDD
@@ -33,20 +46,41 @@ def do_query(df, config_file=None, logger=None, context=None):
     :rtype: list
     """
     fdf = df.withColumn("definition", blank_as_null("definition"))
-    newdf=fdf.filter(fdf.definition.isNotNull()).select(fdf.year, fdf.numWords)
-    articles=newdf.rdd.map(tuple)
-    # [(year, (1, num_words)), ...]
-    #print("-------- %s ------" %articles.collect())
-    counts = articles.map(lambda document:
-                           (document[0],
-                            (1, int(document[1]))))
-    ## [(year, (num_documents, num_words)), ...]
-    # =>
-    ## [(year, [num_documents, num_words]), ...]
+    newdf=fdf.filter(fdf.definition.isNotNull()).select(fdf.year, fdf.volume, fdf.numPages, fdf.numWords)
 
-    result = counts \
-        .reduceByKey(lambda x, y:
-                     tuple(i + j for i, j in zip(x, y))) \
-        .map(lambda year_data: (year_data[0], list(year_data[1]))) \
-        .collect()
-    return result
+    sqlTrans = SQLTransformer(
+        statement="SELECT year, volume, int(numPages), int(numWords) FROM __THIS__")
+    newdf=sqlTrans.transform(newdf)
+    ##### Number of Words
+
+    num_words= newdf.groupBy("year").sum("numWords").withColumnRenamed("sum(numWords)", "tNumWords")
+    #print("-------Num Words %s ----" % num_words.show())
+    ### Num of Volumes ###
+    df_groups= newdf.groupBy("year", "volume", "numPages").count()
+    #print("-------Groupings %s ----" % df_groups.show())
+
+    num_terms=df_groups.groupBy("year").sum("count").withColumnRenamed("sum(count)", "tNumTerms")
+    #print("-------NumTerms %s ----" % num_terms.show())
+
+    num_pages= df_groups.groupBy("year").sum("numPages").withColumnRenamed("sum(numPages)", "tNumPages")
+    #print("-------NumPages %s ----" % num_pages.show())
+
+    num_vol= df_groups.groupBy("year").count().withColumnRenamed("count", "tNumVol")
+    #print("-------NumVolumes %s ----" % num_vol.show())
+
+    
+    
+    vol_pages=num_vol.join(num_pages, on=["year"],how="inner")
+    vol_pages_terms=vol_pages.join(num_terms, on=["year"],how="inner")
+    result=vol_pages_terms.join(num_words, on=["year"],how="inner")
+    result=result.toPandas()
+    r={}
+    for index, row in result.iterrows():
+        year=row['year']
+        r[year]=[]
+        r[year].append(row["tNumVol"])
+        r[year].append(row["tNumPages"])
+        r[year].append(row["tNumTerms"])
+        r[year].append(row["tNumWords"])
+      
+    return r
